b1185c5e90a667fefbae6b4013704464bd7e72fc
pilotns <pilot.ns@icloud.com>
2017-04-13 11:45:10 +0300

    AMPObjCStudy - initial commit, project created

---
+++ AMPObjCStudy/AMPObjCStudy/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AMPObjCStudy
+//
+//  Created by pilotns on 13.04.17.
+//  Copyright © 2017 pilotns. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+
+    }
+    return 0;
+}
